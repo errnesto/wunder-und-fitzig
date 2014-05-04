c58410570abfee93f5d37d8eb4b95a0d5414a38e
Manuel Reich <errnesto@me.com>
2014-05-04 15:37:35 +0200

    stories left right function and animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -113,10 +113,12 @@ var StoriesContainer = React.createClass({displayName: 'StoriesContainer',
 	getInitialState: function() {
 		return {
 			stories: [],
+			currentStorie: 0,
+			currentItems: []
 		};
 	},
 	componentDidMount: function() {
-		//get news feed from facebook
+		//get stories from Server
 		request
 			.get(this.props.url)
 
@@ -126,16 +128,39 @@ var StoriesContainer = React.createClass({displayName: 'StoriesContainer',
 				});
 			}.bind(this));
 	},
+	slide: function (e) {
+		var tempCurrentItems = this.state.currentItems,
+			numberOfItems = this.state.stories[this.state.currentStorie].items.length,
+			currentItem = tempCurrentItems[this.state.currentStorie] || 0;
+
+		if (e.target.text == 'next') {
+			currentItem ++;
+		} else {
+			currentItem --;
+		}
+		if (currentItem > numberOfItems -1) currentItem = 0;
+
+		if (currentItem >= 0) {
+			tempCurrentItems[this.state.currentStorie] = currentItem;
+			this.setState({
+				currentItems: tempCurrentItems
+			});
+		}
+	},
 	render: function() {
-		var Stories = this.state.stories.map(function (storie, index) {
-			return Storie( 
+		var Stories = this.state.stories.map(function (storie, i) {
+			return (
+				Storie( 
 				{coverBackground:storie.cover_background, 
 				coverImage:storie.cover, 
 				coverText:storie.cover_text, 
-				items:storie.stories}, 
+				items:storie.items,
+				currentItem:this.state.currentItems[i] || 0,
+				slide:this.slide}, 
 					storie.customer
-				);
-		});
+				)
+			);
+		},this);
 		return (
 			React.DOM.section( {className:"stories-container"}, 
 				Stories
@@ -146,27 +171,85 @@ var StoriesContainer = React.createClass({displayName: 'StoriesContainer',
 
 var Storie = React.createClass({displayName: 'Storie',
 	render: function() {
-		var StorieItems = this.props.items.map(function (item) {
-			return (
-					React.DOM.div( {className:"storie-item", style:{'background-image': item.image}}, 
-						React.DOM.p( {className:"storie-text"}, 
-							item.text
-						)
-					)
-				);
-		});
+		var prevNum = this.props.currentItem -1,
+			activeNum = this.props.currentItem,
+			nextNum = this.props.currentItem +1,
+			next = 'next';
+
+		if (prevNum < 0) prevNum = this.props.items.length -1;
+		if (nextNum > this.props.items.length -1) {
+			nextNum = 0;
+			next = 'wraparound';
+		}
+
+		var prevItem = this.props.items[prevNum],
+			activeItem = this.props.items[activeNum],
+			nextItem = this.props.items[nextNum];
+
 		return (
 			React.DOM.div( {className:"storie"} , 
+				StorieItem( 
+					{key:'item-'+prevNum, 
+					pos:"prev", 
+					isCover:prevItem.is_cover, 
+					image:prevItem.image, 
+					text:prevItem.text, 
+					background:prevItem.background}),
+				StorieItem( 
+					{key:'item-'+activeNum, 
+					pos:"active", 
+					isCover:activeItem.is_cover, 
+					image:activeItem.image, 
+					text:activeItem.text, 
+					background:activeItem.background}),
+				StorieItem( 
+					{key:'item-'+nextNum, 
+					pos:next, 
+					isCover:nextItem.is_cover, 
+					image:nextItem.image, 
+					text:nextItem.text, 
+					background:nextItem.background}),
+					
 				React.DOM.h2( {className:"storie-title"}, 
-					this.props.children
+					this.props.children, " ", this.props.currentItem
 				),
-				React.DOM.div( {className:"storie-items-wrapper"}, 
-					React.DOM.div( {className:"cover storie-item", style:{'background': this.props.coverBackground}}, 
-						React.DOM.img( {src:this.props.coverImage} )
-					),
-					StorieItems
+				React.DOM.a( {className:"prev arrow", onClick:this.props.slide}, "prev"),
+				React.DOM.a( {className:"next arrow", onClick:this.props.slide}, "next")
+			)
+		);
+	}
+});
+
+var StorieItem = React.createClass({displayName: 'StorieItem',
+	render: function() {
+		if (this.props.isCover){
+			return this.transferPropsTo(StorieCover(null));
+		} else {
+			return this.transferPropsTo(StoriePage(null));
+		}
+	}
+});
+
+var StorieCover = React.createClass({displayName: 'StorieCover',
+	render: function() {
+		return (
+			React.DOM.div( {className:'cover storie-item ' + this.props.pos, style:{'background': this.props.background}}, 
+				React.DOM.img( {src:this.props.image} ),
+				React.DOM.p( {className:"storie-text"}, 
+					this.props.text
 				)
-				
+			)
+		);
+	}
+});
+
+var StoriePage = React.createClass({displayName: 'StoriePage',
+	render: function() {
+		return (
+			React.DOM.div( {className:'storie-item ' + this.props.pos, style:{'background-image': this.props.image}}, 
+				React.DOM.p( {className:"storie-text"}, 
+					this.props.text
+				)
 			)
 		);
 	}
--- src/react/stories.jsx
+++ src/react/stories.jsx
@@ -2,10 +2,12 @@ var StoriesContainer = React.createClass({
 	getInitialState: function() {
 		return {
 			stories: [],
+			currentStorie: 0,
+			currentItems: []
 		};
 	},
 	componentDidMount: function() {
-		//get news feed from facebook
+		//get stories from Server
 		request
 			.get(this.props.url)
 
@@ -15,16 +17,39 @@ var StoriesContainer = React.createClass({
 				});
 			}.bind(this));
 	},
+	slide: function (e) {
+		var tempCurrentItems = this.state.currentItems,
+			numberOfItems = this.state.stories[this.state.currentStorie].items.length,
+			currentItem = tempCurrentItems[this.state.currentStorie] || 0;
+
+		if (e.target.text == 'next') {
+			currentItem ++;
+		} else {
+			currentItem --;
+		}
+		if (currentItem > numberOfItems -1) currentItem = 0;
+
+		if (currentItem >= 0) {
+			tempCurrentItems[this.state.currentStorie] = currentItem;
+			this.setState({
+				currentItems: tempCurrentItems
+			});
+		}
+	},
 	render: function() {
-		var Stories = this.state.stories.map(function (storie, index) {
-			return <Storie 
+		var Stories = this.state.stories.map(function (storie, i) {
+			return (
+				<Storie 
 				coverBackground={storie.cover_background} 
 				coverImage={storie.cover} 
 				coverText={storie.cover_text} 
-				items={storie.stories}>
+				items={storie.items}
+				currentItem={this.state.currentItems[i] || 0}
+				slide={this.slide}>
 					{storie.customer}
-				</Storie>;
-		});
+				</Storie>
+			);
+		},this);
 		return (
 			<section className="stories-container">
 				{Stories}
@@ -35,27 +60,85 @@ var StoriesContainer = React.createClass({
 
 var Storie = React.createClass({
 	render: function() {
-		var StorieItems = this.props.items.map(function (item) {
-			return (
-					<div className="storie-item" style={{'background-image': item.image}}>
-						<p className="storie-text">
-							{item.text}
-						</p>
-					</div>
-				);
-		});
+		var prevNum = this.props.currentItem -1,
+			activeNum = this.props.currentItem,
+			nextNum = this.props.currentItem +1,
+			next = 'next';
+
+		if (prevNum < 0) prevNum = this.props.items.length -1;
+		if (nextNum > this.props.items.length -1) {
+			nextNum = 0;
+			next = 'wraparound';
+		}
+
+		var prevItem = this.props.items[prevNum],
+			activeItem = this.props.items[activeNum],
+			nextItem = this.props.items[nextNum];
+
 		return (
 			<div className="storie" >
+				<StorieItem 
+					key={'item-'+prevNum} 
+					pos="prev" 
+					isCover={prevItem.is_cover} 
+					image={prevItem.image} 
+					text={prevItem.text} 
+					background={prevItem.background}/>
+				<StorieItem 
+					key={'item-'+activeNum} 
+					pos="active" 
+					isCover={activeItem.is_cover} 
+					image={activeItem.image} 
+					text={activeItem.text} 
+					background={activeItem.background}/>
+				<StorieItem 
+					key={'item-'+nextNum} 
+					pos={next} 
+					isCover={nextItem.is_cover} 
+					image={nextItem.image} 
+					text={nextItem.text} 
+					background={nextItem.background}/>
+					
 				<h2 className="storie-title">
-					{this.props.children}
+					{this.props.children} {this.props.currentItem}
 				</h2>
-				<div className="storie-items-wrapper">
-					<div className="cover storie-item" style={{'background': this.props.coverBackground}}>
-						<img src={this.props.coverImage} />
-					</div>
-					{StorieItems}
-				</div>
-				
+				<a className="prev arrow" onClick={this.props.slide}>prev</a>
+				<a className="next arrow" onClick={this.props.slide}>next</a>
+			</div>
+		);
+	}
+});
+
+var StorieItem = React.createClass({
+	render: function() {
+		if (this.props.isCover){
+			return this.transferPropsTo(<StorieCover></StorieCover>);
+		} else {
+			return this.transferPropsTo(<StoriePage></StoriePage>);
+		}
+	}
+});
+
+var StorieCover = React.createClass({
+	render: function() {
+		return (
+			<div className={'cover storie-item ' + this.props.pos} style={{'background': this.props.background}}>
+				<img src={this.props.image} />
+				<p className="storie-text">
+					{this.props.text}
+				</p>
+			</div>
+		);
+	}
+});
+
+var StoriePage = React.createClass({
+	render: function() {
+		return (
+			<div className={'storie-item ' + this.props.pos} style={{'background-image': this.props.image}}>
+				<p className="storie-text">
+					{this.props.text}
+				</p>
 			</div>
 		);
 	}
